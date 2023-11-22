6cc636dd3fee55756b4f3623e0323d9c3198a05d
Alexander Argunov <alexander.argunov@tern.com>
2023-11-22 13:21:25 +0300

    sfsadf

--- functions/api.ts
+++ functions/api.ts
@@ -3,12 +3,10 @@ import { drizzle } from "drizzle-orm/neon-serverless";
 
 import { users } from "../db/schema";
 
-export default {
-  async fetch(req, env, ctx) {
-    const pool = new Pool({ connectionString: env.DATABASE_URL });
-    const db = drizzle(pool);
-    // const result = await db.select().from(users);
-    ctx.waitUntil(pool.end());
-    return new Response("asdf");
-  },
-};
+export async function onRequest(ctx) {
+  const pool = new Pool({ connectionString: env.DATABASE_URL });
+  const db = drizzle(pool);
+  // const result = await db.select().from(users);
+  ctx.waitUntil(pool.end());
+  return new Response("asdf");
+}
