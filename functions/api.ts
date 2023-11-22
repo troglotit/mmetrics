3a9780ca4febe90b86f0791f37dcaf3c2187056d
Alexander Argunov <alexander.argunov@tern.com>
2023-11-22 15:51:02 +0300

    asdf

--- functions/api.ts
+++ functions/api.ts
@@ -4,9 +4,13 @@ import { drizzle } from "drizzle-orm/neon-serverless";
 import { users } from "../db/schema";
 
 export async function onRequest(ctx) {
-  const pool = new Pool({ connectionString: env.DATABASE_URL });
+  // const pool = new Pool({ connectionString: env.DATABASE_URL });
+  const pool = new Pool({
+    connectionString:
+      "postgresql://troglotit:5OadCe4IMBcm@ep-noisy-thunder-67095905-pooler.eu-central-1.aws.neon.tech/allmagen?sslmode=require",
+  });
   const db = drizzle(pool);
-  // const result = await db.select().from(users);
+  const result = await db.select().from(users);
   ctx.waitUntil(pool.end());
-  return new Response("asdf");
+  return new Response(JSON.stringify(result));
 }
